9ad36947bf9d6bd4492bbf4938ab6f7988dc253c
Patsy <patsyesaurer@gmail.com>
2024-01-28 09:05:28 -0800

    Added mock data to the front-end structure

---
+++ src/mockCakes.js
@@ -0,0 +1,45 @@
+let mockCakes = [
+  {
+    id: 1,
+    cakeName: "Honey Cake",
+    cakeDescription:
+      "Sweet honey cake buzzing with flavor and balanced with a honey cream cheese icing and honey glaze!",
+    cakeIngredients:
+      "170 g Honey, 8 g Vanilla, 4 Eggs, 300 g Buttermilk, 168 g Sugar, 226 g Butter, 400 g AP Flour, 7 g Baking Powder, 5 g Salt, 1 tsp Cinnamon, 1/4 tsp Nutmeg, 1/8 tsp Ginger, 1/8 tsp Cloves",
+    cakeInstructions:
+      "1. Preheat the oven to 350°F. 2. Line two 9” cake pans with parchment paper. 3. In a large bowl, sift together the dry ingredients. 4. In a bowl of a stand mixer fitted with a paddle attachment, cream together the butter and sugar. 5. Then add in the eggs, honey, and vanilla and combine. 6. Alternate the dry ingredients with the buttermilk until combined. 7. Pour the batter evenly into the prepared cake pans. 8. Bake for 30 minutes or until a cake pin comes out clean. 9. Cool on a wire rack for about 15 minutes before removing.",
+    cakeServings: 12,
+    cakeImage:
+      "https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706415571/honey-cake_hcmhqo.jpg",
+    user_id: 1,
+  },
+  {
+    id: 2,
+    cakeName: "Birthday Cake",
+    cakeDescription:
+      "Vanilla cake filled and frosted with vanilla cream cheese icing and topped with sprinkles!",
+    cakeIngredients:
+      "350 g AP flour, 8 g Baking powder, 3.7 g Salt, 227 g Butter, softened, 417 g Sugar, 5 Eggs, 2 tsp Vanilla extract, 270 ml	Buttermilk",
+    cakeInstructions:
+      "1. Preheat the oven to 375°F. 2. Grease and line two 9” cake pans with parchment paper. 3. In a stand mixer fitted with a paddle attachment, cream together the butter and sugar until fluffy. 4. Scrape down the sides and beat in the eggs one at a time until incorporated, then add in vanilla. 5. With the mixer on the lowest speed, gradually add the dry ingredients alternating with the buttermilk and mix until smooth and creamy. 6. Pour the batter into the prepared pans, making sure to scale each one evenly. 7. Bake for 28-30 minutes or until the cake tester comes out clean. Allow the cakes to rest in the pans for 10 minutes. 8. Run a knife around the edges to loosen the cake and invert it onto a cooling rack. 9. Let the cakes cool completely before slicing and filling.",
+    cakeServings: 12,
+    cakeImage:
+      "https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706416608/bday-cake_u6ivhn.jpg",
+    user_id: 1,
+  },
+  {
+    id: 2,
+    cakeName: "Grapefruit Cake",
+    cakeDescription:
+      "An elegant grapefruit poppy seed cake layered with fresh grapefruit curd and a light vanilla buttercream topped with white chocolate glaze and candied grapefruit garnish.",
+    cakeIngredients:
+      "325 g Sugar, 180 g	Egg whites(about 6 eggs), 240 g	Sour cream, 5 g	Vanilla extract, 230 g	Grapefruit juice, 393 g	Cake flour, 6 g	Baking powder, 5 g Salt, 27 g Poppy seeds",
+    cakeInstructions:
+      "1. Preheat the oven to 325°F. 2. Grease two 9” cake pans and line with parchment paper. Set aside. 3. In a medium bowl, whisk together the cake flour, baking powder, salt and poppy seeds. Set aside. 4. In a bowl of a stand mixer, fitted with a paddle attachment, cream together the butter and sugar until light and fluffy, about 2 minutes. Scrape down the sides. 5. Add the egg whites on medium speed, scraping down the sides if needed. 6. Add in the sour cream and vanilla extract and beat until incorporated, about one minute. 7. With the mixer on the lowest speed, alternate the flour mixture with the grapefruit juice until incorporated. Scrape down the sides and mix for 30 seconds. 8. Pour the batter evenly using a scale into the prepare cake pans and even out the top batter. 9. Bake for 28-30 minutes or until a cake tester comes out clean. Cool on a wire rack for 15 minutes, then run a knife around the cakes and remove. Wrap in plastic and chill in the freezer or refrigerator until ready to frost and decorate.",
+    cakeServings: 12,
+    cakeImage:
+      "https://res.cloudinary.com/dl11m63ow/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1706416401/grapefruit-cake_qae1rt.jpg",
+    user_id: 2,
+  },
+];
+export default mockCakes;
---
+++ src/mockUsers.js
@@ -0,0 +1,15 @@
+let mockUsers = [
+  {
+    id: 1,
+    email: "test1@example.com",
+  },
+  {
+    id: 2,
+    email: "test2@example.com",
+  },
+  {
+    id: 3,
+    email: "test3@example.com",
+  },
+];
+export default mockUsers;
