e7cd9f7d43834d3481d739ba67c818af884df5e3
Patsy <patsyesaurer@gmail.com>
2024-01-28 11:57:05 -0800

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Home from './pages/Home.js'
+import SignUp from './pages/SignUp.js'
+import SignIn from './pages/SignIn.js'
+import CakeIndex from './pages/CakeIndex.js'
+import CakeProtectedIndex from './pages/CakeProtectedIndex.js'
+import CakeShow from './pages/CakeShow.js'
+import CakeNew from './pages/CakeNew.js'
+import CakeEdit from './pages/CakeEdit.js'
+import NotFound from './pages/NotFound.js'
 import mockUsers from './mockUsers.js'
 import mockCakes from './mockCakes.js'
 
@@ -8,7 +20,21 @@ const App = () => {
 
   return (
     <>
-      <h1>Cake Recipe App</h1>
+      <Header currentUser={currentUser} />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/index" element={<CakeIndex cakes={cakes}/>} />
+        {currentUser && (
+          <Route path="/mycakes" element={<CakeProtectedIndex currentUser={currentUser} cakes={cakes} />} />
+        )}
+        <Routes path="/show/:id" element={<CakeShow cakes={cakes} />} />
+        <Route path="/new" element={<CakeNew cakes={cakes} />} />
+        <Route path="/edit/:id" element={<CakeEdit cakes={cakes} />}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
